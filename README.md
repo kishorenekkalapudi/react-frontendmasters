215bb93496fe6ec2f5d951cb9744fee5b75ebe00
fimhub <fredim.oxford@gmail.com>
2016-04-07 16:40:52 -1000

    bundle.js

---
+++ README.md
@@ -0,0 +1,14 @@
+
+
+# for webpack to build
+webpack js/ClientApp.js public/bundle.js (put into script)
+
+ls -lsa public/bundle.js (to see size)
+
+# standard
+standard (to require in react)
+
+# test scripts
+npm run test
+npm run build
+
--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,27 +1,19 @@
+var React = require('react')
+var ReactDOM = require('react-dom')
+var MyTitle = require('./MyTitle')
+
 var div = React.DOM.div
-var h1 = React.DOM.h1
 
-//composite component of our own creation
-var MyTitle = React.createClass({
-	render () {
-		return (
-			div(null,
-				h1({style: {color: this.props.color}}, this.props.title)
-			)
-		)
-	}
-})
-
-//create factory
+// create factory
 var MyTitleFact = React.createFactory(MyTitle)
 var ce = React.createElement
 
 var MyFirstComponent = (
   div(null,
-  	MyTitleFact({title: 'Props are great1', color: 'rebeccapurple'}),
-  	React.createElement(MyTitle, {title: 'Use props everywhere', color: 'mediumaquamarine'}),
-  	ce(MyTitle, {title: 'Props are the best', color: 'peru'})
-    //refactor to React.createElement(MyTitle, null)
+  	MyTitleFact({title:'Props are great1',color:'rebeccapurple'}),
+  	React.createElement(MyTitle,{title:'Use props everywhere',color:'mediumaquamarine'}),
+  	ce(MyTitle,{title:'Props are the best',color:'peru'})
+    // refactor to React.createElement(MyTitle, null)
   )
 )
 
