968f0bcb603f0ca68b767969e3c61963162bf98a
fimhub <fredim.oxford@gmail.com>
2016-04-08 21:28:12 -1000

    photos

--- js/Showcard.jsx
+++ js/Showcard.jsx
@@ -2,6 +2,7 @@ const React = require('react')
 
 const ShowCard = (props) => (
 	<div className='show-card'>
+		<img src={'public/img/posters/${props.show.poster}'} className='show-card-img' />
 		<div className='show-card-text'>
 			<h3 className='show-card-title'>{props.show.title}</h3>
 			<h4 className='show-card-year'>({props.show.year})</h4>
