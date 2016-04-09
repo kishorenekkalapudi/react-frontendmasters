daef9ebe77e2c1401b04bb98fa22871110d63560
fimhub <fredim.oxford@gmail.com>
2016-04-08 20:59:50 -1000

    create ShowCard abstraction

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,11 +1,14 @@
 const React = require('react')
+const ShowCard = require('./ShowCard')
 const data = require('../public/data')
 
 const Search = () => (
-	<div className = 'container'>
-		{data.shows.map((show) => (
-			<h3>{show.title}</h3>
-		))}
+	<div className ='container'>
+		<div className='shows'>
+			{data.shows.map((show) => (
+				<ShowCard show={show} />
+			))}
+		</div>
 	</div>
 )
 
