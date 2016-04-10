ccf5db55d82c8bf136230a8d33215da85ec00f5a
fimhub <fredim.oxford@gmail.com>
2016-04-10 10:34:09 -1000

    bind search term

--- js/Search.jsx
+++ js/Search.jsx
@@ -2,18 +2,30 @@ const React = require('react')
 const ShowCard = require('./ShowCard')
 const data = require('../public/data')
 
-const Search = () => (
-	<div className ='container'>
-		<header className='header'>
-			<h1 className='brand'>extortion</h1>
-			<input className='search-input' type='text' placeholder='Search' />
-		</header>
-		<div className='shows'>
-			{data.shows.map((show) => (
-				<ShowCard {...show} key={show.imdbID} />
-			))}
-		</div>
-	</div>
-)
+const Search = React.createClass({
+	getInitialState () {
+		return {
+			searchTerm: 'this is my search term'
+		}
+	},
+	handleSearchTermEvent (event) {
+		this.setState({ searchTerm: event.target.value })
+	},
+	render () {
+		return (
+			<div className ='container'>
+				<header className='header'>
+					<h1 className='brand'>{this.state.searchTerm}</h1>
+					<input value={this.state.searchTerm} className='search-input' type='text' placeholder='Search' onChange={this.handleSearchTermEvent} />
+				</header>
+				<div className='shows'>
+					{data.shows.map((show) => (
+						<ShowCard {...show} key={show.imdbID} />
+					))}
+				</div>
+			</div>
+		)
+	}
+})
 
 module.exports = Search
