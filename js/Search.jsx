2e5d7b0e7d1cde2dff452a70afb6dcca427dc087
fimhub <fredim.oxford@gmail.com>
2016-04-10 10:22:13 -1000

    search bar

--- js/Search.jsx
+++ js/Search.jsx
@@ -4,6 +4,10 @@ const data = require('../public/data')
 
 const Search = () => (
 	<div className ='container'>
+		<header className='header'>
+			<h1 className='brand'>extortion</h1>
+			<input className='search-input' type='text' placeholder='Search' />
+		</header>
 		<div className='shows'>
 			{data.shows.map((show) => (
 				<ShowCard {...show} key={show.imdbID} />
