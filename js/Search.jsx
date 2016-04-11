ded34190f9f8f307e148c7f0c1ba7e6a495a3f09
fimhub <fredim.oxford@gmail.com>
2016-04-11 10:34:16 -1000

    fixed search

--- js/Search.jsx
+++ js/Search.jsx
@@ -5,24 +5,24 @@ const { object, string } = React.PropTypes
 const { connector } = require('./Store')
 
 const Search = React.createClass({
-  propTypes: {
-    route: object,
-    searchTerm: string
-  },
-  render () {
-    return (
-      <div className='container'>
-        <Header showSearch />
-        <div className='shows'>
-          {this.props.route.shows
-            .filter((show) => `${show.title} ${show.description}`.toUpperCase().indexOf(this.props.searchTerm.toUpperCase()) >= 0)
-            .map((show) => (
-              <ShowCard {...show} key={show.imdbID} />
-          ))}
-        </div>
-      </div>
-    )
-  }
+	propTypes: {
+		route: object,
+		searchTerm: string
+	},
+	render () {
+		return (
+			<div className ='container'>
+				<Header showSearch />
+				<div className='shows'>
+					{this.props.route.shows
+						.filter((show) => `${show.title} ${show.description}`.toUpperCase().indexOf(this.props.searchTerm.toUpperCase()) >= 0)
+						.map((show) => (
+						<ShowCard {...show} key={show.imdbID} />
+					))}
+				</div>
+			</div>
+		)
+	}
 })
 
 module.exports = connector(Search)
