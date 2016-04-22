63a4d927e97474f6927a8f061617f715e72ce84b
fimhub <fredim.oxford@gmail.com>
2016-04-21 20:56:33 -1000

    fix error to port

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(express.static('public'));
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'index.html'));
-}).listen(process.env.PORT || 5000);
+}).listen('port');
 
 // app.listen(process.env.PORT || 3000, 'localhost', function(err) {
 //   if (err) {
