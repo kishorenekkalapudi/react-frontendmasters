f06de89ba6deee2b09a8e1113e14e0c2bc8844e9
fimhub <fredim.oxford@gmail.com>
2016-04-21 20:55:10 -1000

    fixing port

--- server.js
+++ server.js
@@ -20,11 +20,7 @@ app.use(express.static('public'));
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'index.html'));
-      var result = 'App is running'
-    	res.send(result);
-}).listen(app.get('port'), function() {
-    console.log('App is running, server is listening on port ', app.get('port'));
-});
+}).listen(process.env.PORT || 5000);
 
 // app.listen(process.env.PORT || 3000, 'localhost', function(err) {
 //   if (err) {
